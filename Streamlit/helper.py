10baec13c3102ef517205b4022d9583053acc46b
Kusal <kusal@Kusals-MacBook-Air.local>
2026-01-12 22:36:27 +0530

    Ensure NLTK stopwords available on Streamlit Cloud

--- Streamlit/helper.py
+++ Streamlit/helper.py
@@ -4,6 +4,8 @@ import distance
 from fuzzywuzzy import fuzz
 import pickle
 import numpy as np
+import nltk
+nltk.download("stopwords")
 from nltk.corpus import stopwords
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
